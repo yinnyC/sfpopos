df9601d94656190a551cf74564bdc62e61dc234d
Yin <yinnnn17@gmail.com>
2020-11-16 16:48:12 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,18 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+      <div className="Title-Subtitle">
+        <p>San Francisco Privately Owned Public Open Spaces</p>
+      </div> 
+    </div>
+    
+  )
+}
+
+export default Title
