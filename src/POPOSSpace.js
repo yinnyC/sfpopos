7d869a3823a9cd56c062e517bda6ecd7ba556029
Yin <yinnnn17@gmail.com>
2020-11-18 21:20:32 -0800

    content component built

---
+++ src/POPOSSpace.js
@@ -0,0 +1,20 @@
+// src/Project.js
+
+import React from 'react'
+
+function POPOSSpace(props) {
+  const { name, image, address } = props
+  return (
+    <div>
+      <img src={`${process.env.PUBLIC_URL}images/${image}`}     
+        width="300" 
+        height="300" 
+        alt='{name}'
+      />
+      <h1>{name}</h1>
+      <div>{address}</div>
+    </div>
+  )
+}
+
+export default POPOSSpace
