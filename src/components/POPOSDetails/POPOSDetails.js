4e4ebeaaab3394269b670c016038858bd144d095
Yin <yinnnn17@gmail.com>
2020-11-21 15:38:28 -0800

    STYLING DETAILS

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -2,27 +2,26 @@
 
 import React from 'react'
 import data from '../../sfpopos-data.json'
+import './POPOSDetails.css'
 
 function POPOSDetails(props) {
   const { id } = props.match.params // Location index
   const { images, title, desc, hours, features, geo } = data[id]
-
   return (
-    <div>
-      <div>
+    <div className="POPOSDetails">
+      <div className="POPOSDetails-image">
         <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title}/>
       </div>
-
-      <div>
-        <h1>{ title }</h1>
-        <p>{ desc }</p>
-        <p>{ hours }</p>
-        <p>{ features }</p>
-        <p>{ geo.lat } { geo.lon }</p>
+      <div className="POPOSDetails-info">
+        <h1 className="POPOSDetails-title">{ title }</h1>
+        <p className="POPOSDetails-desc">{ desc }</p>
+        <p className="POPOSDetails-hours">{ hours }</p>
+        <p className="POPOSDetails-features">{ features }</p>
+        <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
       </div>
-
     </div>
   )
 }
+
 
 export default POPOSDetails
