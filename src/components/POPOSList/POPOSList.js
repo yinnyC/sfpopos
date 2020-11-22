4cda5fbbb98aaf4c8aa7cee10ff6e1a005c1d2b8
Yin <yinnnn17@gmail.com>
2020-11-21 16:39:40 -0800

    adding searching space

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,13 +1,20 @@
 // src/POPOSList.js
 
-import React from 'react';
+import React, { useState } from 'react'
 import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import data from '../../sfpopos-data.json'
 import './POPOSList.css';
 
 function POPOSList() {
-
-  const spaces = data.map(({ title, address, images, hours }, i) => {
+  const [ query, setQuery ] = useState('')
+  const spaces = data.filter((obj) => {
+    // true if query is in title
+    const inTitle = obj.title.toLowerCase().includes(query.toLowerCase())
+    // true if query is in address
+    const inAddress = obj.address.toLowerCase().includes(query.toLowerCase())
+    // return true if either is true
+    return inTitle || inAddress
+}).map(({ title, address, images, hours }, i) => {
   return (
     <POPOSSpace
       id={i}
@@ -21,8 +28,18 @@ function POPOSList() {
 })
 
   return (
-    <div className="POPOSList">
-      { spaces }
+    <div>
+       <form>
+                <input 
+                    value={query}
+                    placeholder="search"
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        <div className="POPOSList">
+        { spaces }
+        </div>
     </div>
   )
 }
