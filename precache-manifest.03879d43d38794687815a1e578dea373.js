cd1e9de577f428cf3334a022a1ddc35c07afba3c
Yin <yinnnn17@gmail.com>
2021-03-07 22:30:14 -0800

    Updates

---
+++ precache-manifest.03879d43d38794687815a1e578dea373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1a9799f5f9f838e1d0d1ae298c55b3",
+    "url": "/sfpopos/index.html"
+  },
+  {
+    "revision": "dd5d34763efb89624943",
+    "url": "/sfpopos/static/css/main.9cb3d953.chunk.css"
+  },
+  {
+    "revision": "17c2be1fdd39cf6089d8",
+    "url": "/sfpopos/static/js/2.af7747c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sfpopos/static/js/2.af7747c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5d34763efb89624943",
+    "url": "/sfpopos/static/js/main.4eac2e84.chunk.js"
+  },
+  {
+    "revision": "9ffae97291798d379b47",
+    "url": "/sfpopos/static/js/runtime-main.d61decc5.js"
+  }
+]);
